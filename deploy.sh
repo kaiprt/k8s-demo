0a19cce1c581979ff48eff0261e30fe6c8f3d74b
Kai Prout <kai@moonrake.it>
2019-09-29 21:32:37 -0400

    Updating travis yaml, adding deploy.sh and updating images for deployment files.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t moonrake/multi-client:latest -t moonrake/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t moonrake/multi-server:latest -t moonrake/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t moonrake/multi-worker:latest -t moonrake/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker push moonrake/multi-client:latest
+docker push moonrake/multi-server:latest
+docker push moonrake/multi-worker:latest
+
+docker push moonrake/multi-client:latest$GIT_SHA
+docker push moonrake/multi-server:latest$GIT_SHA
+docker push moonrake/multi-worker:latest$GIT_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=moonrake/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment server=moonrake/multi-server:$GIT_SHA
+kubectl set image deployments/worker-deployment server=moonrake/multi-server:$GIT_SHA
