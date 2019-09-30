be575816f8b4b11fdedd5f6a45ffcb3db7f3324d
Kai Prout <kai@moonrake.it>
2019-09-29 22:11:19 -0400

    Debugging deploy.sh now.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 docker build -t moonrake/multi-client:latest -t moonrake/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t moonrake/multi-server:latest -t moonrake/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t moonrake/multi-worker:latest -t moonrake/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
-docker push moonrake/multi-client:latest
-docker push moonrake/multi-server:latest
-docker push moonrake/multi-worker:latest
+# docker push moonrake/multi-client:latest
+# docker push moonrake/multi-server:latest
+# docker push moonrake/multi-worker:latest
 
 docker push moonrake/multi-client:latest$GIT_SHA
 docker push moonrake/multi-server:latest$GIT_SHA
